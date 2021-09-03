34de81eeb6cc6c463757d60e7f1d9587776ee476
Cam Tomlin <stouthalfling3@Cams-MacBook-Air.local>
2021-09-03 12:04:50 -0600

    added watercoller component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
-import { Routes, RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
 import { IndexComponent } from "./pages/index/index.component";
 import { ProfilepageComponent } from "./pages/examples/profilepage/profilepage.component";
 import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
 import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
 import { EventspageComponent } from "./pages/examples/eventspage/eventspage.component";
+import { WatercoolerpageComponent } from "./pages/examples/watercoolerpage/watercoolerpage.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
@@ -16,15 +17,13 @@ const routes: Routes = [
   { path: "register", component: RegisterpageComponent },
   { path: "landing", component: LandingpageComponent },
   { path: "events", component: EventspageComponent},
+  { path: "watercooler", component: WatercoolerpageComponent},
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes, {
-      useHash: true
-    })
   ],
   exports: []
 })
