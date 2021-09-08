3ea19f42718e4774c4a3fe00de434b4da4b0d754
Cam <ctvaan@gmail.com>
2021-09-08 12:56:12 -0600

    second commit of rebuild, add all auth tokens, added user, moderator, admin components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModel } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,12 @@ import { PricingComponent } from './pricing/pricing.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
 import { WatercoolerComponent } from './home/watercooler/watercooler.component';
+import { LoginComponent } from './home/login/login.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +25,19 @@ import { WatercoolerComponent } from './home/watercooler/watercooler.component';
     RegisterComponent,
     ContactComponent,
     WatercoolerComponent,
+    LoginComponent,
+    BoardUserComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgModel
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
