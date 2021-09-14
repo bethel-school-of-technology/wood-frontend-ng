1b7ffee381ce5cd6750c8d4bdd431b297c223d76
Caleb <calebjbeals@gmail.com>
2021-09-14 17:53:39 -0500

    code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { UsersService } from '../services/users.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class HomeComponent implements OnInit {
+export class LoginComponent implements OnInit {
+  getUser: User = new User()
 
-  constructor() { }
+
+  constructor(private userService: UsersService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  User(){
+    this.userService.getUser(this.getUser).subscribe((userLoggedIn) => {
+      this.getUser = userLoggedIn;
+      console.log(userLoggedIn);
+      this.router.navigate(["login"])
+    })
+  }
 }
+
+
